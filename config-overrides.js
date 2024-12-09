f9bb513f8b69c3262cf6dbe5845e86ab8eb84391
Basil Udo <basil@xeddy.app>
2024-12-09 15:21:46 -0500

    update to config override

--- config-overrides.js
+++ config-overrides.js
@@ -1,7 +1,16 @@
+const webpack = require('webpack');
+
 module.exports = function override(config, env) {
     config.resolve.fallback = {
       fs: false,
       path: false,
+      crypto: false,
     };
+    config.plugins = [
+      new webpack.DefinePlugin({
+        'process.env.REACT_APP_BACKEND_PROD_URL': JSON.stringify(process.env.REACT_APP_BACKEND_PROD_URL),
+        'process.env.REACT_APP_BACKEND_URL': JSON.stringify(process.env.REACT_APP_BACKEND_URL),
+      }),
+    ];
     return config;
   };
